9e984b479735b2e9922183c8d628068de935956b
Ant0in <antoine.berthion@ulb.be>
2024-09-28 20:08:41 +0200

    pipeline test

--- modules/whois/__init__.py
+++ modules/whois/__init__.py
@@ -7,7 +7,7 @@ class WhoisLookupModule:
     def __init__(self, url: str) -> None:
         self.url: str = url
 
-    def run(self) -> ...:
+    def run(self, dependencies: list = None) -> ...:
         # Fetch WHOIS data from the DNS
         w = whois.whois(self.url)
-        return w
+        return [w]
--- test_main.py
+++ test_main.py
@@ -1,11 +1,17 @@
+
 from modules.legal_keywords_checker import LegalKeywordsChecker
 from modules.trustpilot_checker import TrustPilotReviews
+from modules.whois import WhoisLookupModule
+from modules.update_date import UpdateDateModule
+
 from pipeline import Pipeline
-from executor import Executor, ParallelExecutor
+from executor import Executor
 
 pipeline = Pipeline()
 pipeline.add_module("TrustPilotChecker", [])
 pipeline.add_module("LegalChecker", [])
+pipeline.add_module("WhoIS", [])
+pipeline.add_module("UpdateDate", ["WhoIS"])
 
 
 url: str = 'https://google.com'
@@ -13,6 +19,8 @@ url: str = 'https://google.com'
 module_list = {
     "TrustPilotChecker": TrustPilotReviews(url),
     "LegalChecker": LegalKeywordsChecker(url),
+    "WhoIS": WhoisLookupModule(url),
+    "UpdateDate": UpdateDateModule(),
 }
 
 executor = Executor()
