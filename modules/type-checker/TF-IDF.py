9de0f37b4326aceea6239320204430817decbfb9
Ant0in <antoine.berthion@ulb.be>
2024-09-28 14:39:36 +0200

    tfidk sklearn

--- modules/type-checker/TF-IDF.py
+++ modules/type-checker/TF-IDF.py
@@ -1,62 +1,33 @@
 
 from sklearn.feature_extraction.text import TfidfVectorizer
+import nltk
+from numpy import ndarray
 
 class TFIDF:
+
+    @staticmethod
+    def caculateTFIDFvec(doc_corpus: list[list[str]], language: str = 'english') -> dict:
+        # Vectorize doc_corpus using TFIDF algorithm.
+        _stopwords: list[str] = TFIDF.get_stopwords(lang=language)
+        _vectorizer: TfidfVectorizer = TfidfVectorizer(stop_words=_stopwords)
+
+        _tfidf_matrix = _vectorizer.fit_transform(doc_corpus)
+        _terms: ndarray = _vectorizer.get_feature_names_out()
+        _scores: ndarray = _tfidf_matrix.toarray()[0]
+
+        tfidf_scores = {_terms[i]: _scores[i] for i in range(len(_terms))}
+        return tfidf_scores
+
+    @staticmethod
+    def get_stopwords(lang: str) -> list[str]:
+        # Get the stopwords to ignore from given language 'lang'.
+        stw: list[str] = nltk.corpus.stopwords.words(lang)
+        return stw
     
-    @staticmethod
-    def calculateTFvec(word_vec: list[str]) -> dict[str, float]:
+    def download_NLTKstopwords() -> None:
+        # Download the NLTK stopwords. Call only if not downloaded.
+        nltk.download('stopwords')
 
-        _ret: dict[str, float] = dict()
-        for w in word_vec:
-            if w not in _ret: _ret[w] = 1
-            else: _ret[w] += 1
-            
-        wc: int = len(word_vec)
-        assert wc > 0, f'wc = {wc}'
-
-        return {k: (v / wc) for k, v in _ret.items()}
-    
-    @staticmethod
-    def calculateIDF(doc_corpus: list[list[str]]) -> dict[str, float]:
-
-        _idf: dict[str, float] = dict()
-        total_docs = len(doc_corpus)
-        
-        for doc in doc_corpus:
-            for word in set(doc):
-                _idf[word] = _idf.get(word, 0) + 1
-        
-        return {word: log((1 + total_docs / (1 + count)), 10) for word, count in _idf.items()}
-
-    @staticmethod
-    def calculateTFIDFvec(doc_corpus: list[list[str]]) -> dict:
-
-        idf_scores = TFIDF.calculateIDF(doc_corpus)
-        
-        tfidf_corpus = []
-        for doc in doc_corpus:
-            tf_vec = TFIDF.calculateTFvec(doc)
-            tfidf_vec = {word: tf_vec[word] * idf_scores[word] for word in tf_vec}
-            tfidf_corpus.append(tfidf_vec)
-        
-        return tfidf_corpus
-    
-    @staticmethod
-    def mergeTFIDFvec(tfidf_corpus: list[dict]) -> dict:
-        
-        _merged: dict = dict()
-        for tfidfvec in tfidf_corpus:
-            for word, weight in tfidfvec.items():
-                if word in _merged: _merged[word] += weight
-                else: _merged[word] = weight
-        return _merged
-
-    @staticmethod
-    def run(doc_corpus: list[list[str]]) -> dict[str: float]:
-
-        _tfidf_corpus: list[dict] = TFIDF.calculateTFIDFvec(doc_corpus=doc_corpus)
-        _merged_tfidf: dict[str: float] = TFIDF.mergeTFIDFvec(tfidf_corpus=_tfidf_corpus)
-        return _merged_tfidf
 
 
 if __name__ == '__main__':
@@ -68,4 +39,5 @@ if __name__ == '__main__':
         "petit chat se balade dans la prairie"
     ]
 
-    tfidf = TFIDF.run(doc_corpus=[i.split(' ') for i in dc])
+    tfidf = TFIDF.caculateTFIDFvec(dc, 'french')
+    print(tfidf)
