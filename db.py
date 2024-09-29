12b241bcc2c15eeff271d0388905dd2d04a03bfa
Ant0in <antoine.berthion@ulb.be>
2024-09-29 06:41:31 +0200

    db pipeline addon

--- db.py
+++ db.py
@@ -2,11 +2,14 @@ import sqlite3
 import datetime
 
 class DB:
+
     def __init__(self, path):
+        
         self.conn = sqlite3.connect(path)
         self.curr = self.conn.cursor()
 
     def migrate_up(self):
+        
         self.curr.execute("""
         CREATE TABLE websites (
             url STRING PRIMARY KEY,
@@ -17,23 +20,31 @@ class DB:
         self.conn.commit()
     
     def migrate_down(self):
+        
         self.curr.execute("""
         DROP TABLE websites;
         """)
         self.conn.commit()
 
     def reset(self):
+        
         self.migrate_down()
         self.migrate_up()
 
     def put_website(self, url: str, score: float):
+        
         ts = datetime.datetime.now().isoformat()
-
-        self.curr.execute("INSERT INTO websites (url, score, timestamp)  VALUES (?, ?, ?)", (url, score, ts))
+        
+        # Utiliser INSERT OR REPLACE pour mettre à jour si l'entrée existe déjà
+        self.curr.execute("""
+            INSERT OR REPLACE INTO websites (url, score, timestamp) 
+            VALUES (?, ?, ?)
+        """, (url, score, ts))
         self.conn.commit()
 
     def fetch_website(self, url):
-        self.curr.execute("SELECT (score, timestamp) FROM websites WHERE url = ?", (url,))
+        
+        self.curr.execute("SELECT score, timestamp FROM websites WHERE url = ?", (url,))
         result = self.curr.fetchone()
 
         if result is not None:
@@ -42,3 +53,8 @@ class DB:
 
         return None
         
+
+if __name__ == '__main__':
+
+    url: str = 'https://hackyeah.pl/'
+    print(DB(path='./dd.db').fetch_website(url))
